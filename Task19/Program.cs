447e771383123251f50a1aaa20f3d9b945305d73
Denis <kummerd65@gmail.com>
2023-01-20 22:14:13 +0600

    Complete the Task

---
+++ Task19/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+Console.WriteLine("Введите пятизначное число: ");
+string number = Console.ReadLine();
+int lenght = number.Length;
+
+if (lenght== 5)
+{
+    if (number[0] == number[4] && number[1] == number[3])
+    {
+        Console.WriteLine($"{number} - Да. ");
+    } 
+else
+    {
+        Console.WriteLine($"{number} - Нет. ");
+    }
+}
+else
+{
+    Console.WriteLine($"ОШИБКА: {number} - не является пятизначным.");
+}
